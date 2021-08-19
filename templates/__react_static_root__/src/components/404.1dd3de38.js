cc8579e63635e51e08637dc1c588934214a9fb95
FerGonzalezT <fer_180910@hotmail.com>
2021-08-19 02:53:27 -0500

    Updates

---
+++ templates/__react_static_root__/src/components/404.1dd3de38.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{123:function(n,e,a){"use strict";a.r(e);var t=a(0),o=a.n(t);e.default=function(){return o.a.createElement("div",null,o.a.createElement("h1",null,"404 - No se encontro la pagina :("))}}}]);
---
+++ templates/styles.0485ee2d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{206:function(n,o,w){},209:function(n,o,w){}}]);
