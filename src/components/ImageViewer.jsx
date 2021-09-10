dc8c663024c0ebd7258b7bcb03b877f678de6bb6
FerGonzalezT <fer_180910@hotmail.com>
2021-09-10 03:17:55 -0500

    quitar mas placeholder

--- src/components/ImageViewer.jsx
+++ src/components/ImageViewer.jsx
@@ -1,23 +1,16 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import {
-  Grid, Image, Placeholder,
+  Grid, Image,
 } from 'semantic-ui-react';
 
 function ProductImage({ setCurrentImage, image }) {
+  // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState(true);
   return (
     <Grid key={image}>
       <Grid.Row className="clear-padding-bottom">
         <Grid.Column width="16">
-          {
-            loading
-            && (
-              <Placeholder fluid style={{ height: '10vh' }}>
-                <Placeholder.Image />
-              </Placeholder>
-            )
-          }
           <Image
             onClick={() => setCurrentImage(image)}
             style={{ cursor: 'pointer' }}
@@ -39,6 +32,7 @@ ProductImage.propTypes = {
 
 function ImageViewer({ images }) {
   const [currentImage, setCurrentImage] = useState(images[0]);
+  // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState(true);
   return (
     <Grid padded>
@@ -59,14 +53,6 @@ function ImageViewer({ images }) {
           tablet="11"
           mobile="13"
         >
-          {
-            loading
-            && (
-              <Placeholder fluid style={{ height: '30vh' }}>
-                <Placeholder.Image />
-              </Placeholder>
-            )
-          }
           <Image
             src={currentImage}
             fluid
